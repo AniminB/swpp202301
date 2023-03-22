0ff14d2aadeb88b4bcedb2a01293412581d6a645
strikef <seunghyeon.nam@sf.snu.ac.kr>
2023-03-22 15:23:20 +0900

    Update LLVM build scripts

--- practice/install-llvm-mac.sh
+++ practice/install-llvm-mac.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Install necessary dependencies
-# Package names may slightly differ in macOS (brew)
-sudo apt update
-sudo apt -y install git g++ cmake ninja-build python3-distutils zlib1g-dev \
-libncurses-dev
+brew update
+brew install git cmake ninja zlib ncurses
 
 # Download LLVM source
 git clone -b llvmorg-16.0.0 https://github.com/llvm/llvm-project.git --depth 1
@@ -15,17 +13,17 @@ LLVM_DIR=~/llvm-swpp # Edit this directory
 mkdir $LLVM_DIR
 
 # Build LLVM
-# You may erase line 27-28 if your computer is powerful enough
+# Intel Mac users should use X86 instead of AArch64
 cmake -G Ninja -S llvm -B build \
     -DLLVM_ENABLE_PROJECTS="clang;lldb;compiler-rt" \
     -DLLVM_INSTALL_UTILS=ON \
-    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DLLVM_TARGETS_TO_BUILD="AArch64" \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_ENABLE_EH=ON \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_PARALLEL_COMPILE_JOBS=4 \
-    -DLLVM_PARALLEL_LINK_JOBS=1 \
-    -DLLVM_USE_LINKER=gold \
+    -DCOMPILER_RT_ENABLE_IOS=OFF \
+    -DLLDB_INCLUDE_TESTS=OFF \
+    -DLLDB_USE_SYSTEM_DEBUGSERVER=ON \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$LLVM_DIR
